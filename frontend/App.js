6b50e412f74d44560e046b3bcf9d7fd182e70f1d
MovinduSenura <it22371218@my.sliit.lk>
2024-10-08 23:18:22 +0530

    initial commit

---
+++ frontend/App.js
@@ -0,0 +1,11 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center h-screen">
+      <Text className="text-3xl font-bold text-red-500">Open up App.js to start working on your app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
