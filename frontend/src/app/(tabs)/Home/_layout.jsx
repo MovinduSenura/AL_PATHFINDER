26d5c94a8330588f6e574fd3db289b80cdacd728
Osh <107048011+it22368348@users.noreply.github.com>
2024-10-12 07:29:22 +0530

    create consultation Home

--- frontend/src/app/(tabs)/Home/_layout.jsx
+++ frontend/src/app/(tabs)/Home/_layout.jsx
@@ -6,6 +6,64 @@ const StackLayout = () => {
             <Stack.Screen
                 name="index"
                 options={{  headerShown: false }} />
+
+<Stack.Screen
+        name="consultationHome"
+        options={{
+          headerShown: true,
+          title: "Consultation Service",
+          headerBackTitleVisible: false,
+          headerTintColor: "#353333",
+          headerTitleStyle: {
+            color: "#353333",
+            fontWeight: "bold",
+          },
+        }}
+      />
+
+<Stack.Screen
+        name="createAppointment"
+        options={{
+          headerShown: true,
+          title: "Create Appointments",
+          headerBackTitleVisible: false,
+          headerTintColor: "#353333",
+          headerTitleStyle: {
+            color: "#353333",
+            fontWeight: "bold",
+          },
+        }}
+      />
+
+<Stack.Screen
+        name="courseselection"
+        options={{
+          headerShown: true,
+          title: "Course Selection",
+          headerBackTitleVisible: false,
+          headerTintColor: "#353333",
+          headerTitleStyle: {
+            color: "#353333",
+            fontWeight: "bold",
+          },
+        }}
+      />
+
+<Stack.Screen
+        name="degreeselection"
+        options={{
+          headerShown: true,
+          title: "Degree Selection",
+          headerBackTitleVisible: false,
+          headerTintColor: "#353333",
+          headerTitleStyle: {
+            color: "#353333",
+            fontWeight: "bold",
+          },
+        }}
+      />
+
+      
         </Stack>
     )
 }
